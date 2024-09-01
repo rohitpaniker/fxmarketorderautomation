c6b4da9ef2f79779b47c78ee464a2602b59747a3
Rohit Panikar <rohitpanikar@Rohits-MacBook-Air.local>
2024-09-02 01:12:37 +0530

    Add TextArea for adding trades and old dialog inputs for editing trades

--- app/components/dashboard/components/mts-add-trade-dialog.js
+++ app/components/dashboard/components/mts-add-trade-dialog.js
@@ -20,6 +20,7 @@ import {
 import { Loader2 } from "lucide-react"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
+import { Textarea } from "@/components/ui/textarea"
 import { useEffect, useState } from "react"
 
 function CurrencyPairSelectComponent({ selectedOption, isEditMode, defaultCurrency }) {
@@ -91,7 +92,9 @@ function ButtonLoading() {
 
 export function MTSAddTradeDialog({ buttonTitle, isEditMode, dataForEdit }) {
     const [rowId, setRowId] = useState(0);
+    const [textAreaText, setTextAreaText] = useState("");
     const [shouldBeDisalbled, setShouldBeDisalbled] = useState(false);
+    const [isTradeBeingSaved, setIsTradeBeingSaved] = useState(false);
     const [selectedSignalCall, setSelectedSignalCall] = useState("");
     const [selectedCurrency, setSelectedCurrency] = useState("");
     const [entryPrice, setEntryPrice] = useState("");
@@ -116,6 +119,13 @@ export function MTSAddTradeDialog({ buttonTitle, isEditMode, dataForEdit }) {
 
     const saveData = async () => {
         setIsButtonDisabled(true);
+        formatTextArea(textAreaText.split('\n'))
+        setIsTradeBeingSaved(true);
+        // setIsButtonDisabled(false);
+        // setOpen(false);
+    }
+
+    const executeSaveDataAsyncFunc = async () => {
         const response = await fetch("/dashboard/api", {
             method: 'POST',
             headers: {
@@ -129,14 +139,22 @@ export function MTSAddTradeDialog({ buttonTitle, isEditMode, dataForEdit }) {
                     entryPrice,
                     stopLoss,
                     takeProfit,
-                    status: 0
+                    status: false
                 }
             }),
         });
         const data = await response.json();
+
         setIsButtonDisabled(false);
         setOpen(false);
     }
+
+    useEffect(() => {
+        if(isTradeBeingSaved) {
+            executeSaveDataAsyncFunc();
+            setIsTradeBeingSaved(false);
+        }
+    }, [isTradeBeingSaved])
 
     const updateData = async () => {
 
@@ -144,8 +162,6 @@ export function MTSAddTradeDialog({ buttonTitle, isEditMode, dataForEdit }) {
             setOpen(false);
             return false;
         }
-
-        setIsButtonDisabled(true);
 
         const response = await fetch("/dashboard/api", {
             method: 'POST',
@@ -166,8 +182,16 @@ export function MTSAddTradeDialog({ buttonTitle, isEditMode, dataForEdit }) {
             }),
         });
         const data = await response.json();
-        setIsButtonDisabled(false);
-        setOpen(false);
+
+        setIsButtonDisabled(true);
+    }
+
+    const formatTextArea = (data) => {
+        setSelectedCurrency(data[0].toUpperCase())
+        setSelectedSignalCall(data[1].toUpperCase().split(' ').join('_'))
+        setEntryPrice(data[2].split(' ')[1]);
+        setTakeProfit(data[3].split(' ')[1])
+        setStopLoss(data[4].split(' ')[1])
     }
 
     return (
@@ -183,77 +207,65 @@ export function MTSAddTradeDialog({ buttonTitle, isEditMode, dataForEdit }) {
                     </DialogDescription>
                 </DialogHeader>
                 <div className="grid gap-4 py-4">
-                    <div className="grid grid-cols-4 items-center gap-4">
-                        <Label htmlFor="currencyPair" className="text-right">
-                            Pair
-                        </Label>
-                        <CurrencyPairSelectComponent selectedOption={setSelectedCurrency} isEditMode={isEditMode} defaultCurrency={dataForEdit?.pair} />
-                    </div>
-                    {/* <div className="grid grid-cols-4 items-center gap-4">
-                        <Label htmlFor="signalCode" className="text-right">
-                            Signal
-                        </Label>
-                    </div> */}
-                    <div className="grid grid-cols-4 items-center gap-4">
-                        <Label htmlFor="currencyPair" className="text-right">
-                            Order
-                        </Label>
-                        <BuySellStopSelectComponent selectedBuySellSignal={setSelectedSignalCall} isEditMode={isEditMode} defaultSignalValue={dataForEdit?.signal.split(' ').join('_')} />
-                    </div>
-                    <div className="grid grid-cols-4 items-center gap-4">
-                        <Label htmlFor="currencyPair" className="text-right">
-                            Entry Price
-                        </Label>
-                        <Input
-                            onChange={e => setEntryPrice(e.target.value)}
-                            defaultValue={entryPrice}
-                            id="entryPoint"
-                            className="col-span-3"
-                        />
-                    </div>
-                    {/* <div className="grid grid-cols-12 items-center gap-4">
-                        <div className="col-span-4">
-                            <BuySellStopSelectComponent selectedBuySellSignal={setSelectedSignalCall} isEditMode={isEditMode} defaultSignalValue={dataForEdit?.signal.split(' ').join('_')} />
+                    {isEditMode ?
+                        <div className="grid gap-4 py-4">
+                            <div className="grid grid-cols-4 items-center gap-4">
+                                <Label htmlFor="currencyPair" className="text-right">
+                                    Pair
+                                </Label>
+                                <CurrencyPairSelectComponent selectedOption={setSelectedCurrency} isEditMode={isEditMode} defaultCurrency={dataForEdit?.pair} />
+                            </div>
+                            <div className="grid grid-cols-4 items-center gap-4">
+                                <Label htmlFor="currencyPair" className="text-right">
+                                    Order
+                                </Label>
+                                <BuySellStopSelectComponent selectedBuySellSignal={setSelectedSignalCall} isEditMode={isEditMode} defaultSignalValue={dataForEdit?.signal.split(' ').join('_')} />
+                            </div>
+                            <div className="grid grid-cols-4 items-center gap-4">
+                                <Label htmlFor="currencyPair" className="text-right">
+                                    Entry Price
+                                </Label>
+                                <Input
+                                    onChange={e => setEntryPrice(e.target.value)}
+                                    defaultValue={entryPrice}
+                                    id="entryPoint"
+                                    className="col-span-3"
+                                />
+                            </div>
+                            <div className="grid grid-cols-4 items-center gap-4">
+                                <Label htmlFor="stopLoss" className="text-right">
+                                    Stop Loss
+                                </Label>
+                                <Input
+                                    onChange={e => setStopLoss(e.target.value)}
+                                    id="stopLoss"
+                                    defaultValue={stopLoss}
+                                    className="col-span-3"
+                                />
+                            </div>
+                            <div className="grid grid-cols-4 items-center gap-4">
+                                <Label htmlFor="takeProfit" className="text-right">
+                                    Take Profit
+                                </Label>
+                                <Input
+                                    onChange={e => setTakeProfit(e.target.value)}
+                                    id="takeProfit"
+                                    defaultValue={takeProfit}
+                                    className="col-span-3"
+                                />
+                            </div>
                         </div>
-                        <Input
-                            id="selectedCurrencyPair"
-                            defaultValue={selectedCurrency}
-                            value={selectedCurrency}
-                            className="col-span-4"
-                            disabled
-                        />
-                        <Input
-                            onChange={e => setEntryPrice(e.target.value)}
-                            defaultValue={entryPrice}
-                            id="entryPoint"
-                            className="col-span-4"
-                        />
-                    </div> */}
-                    <div className="grid grid-cols-4 items-center gap-4">
-                        <Label htmlFor="stopLoss" className="text-right">
-                            Stop Loss
-                        </Label>
-                        <Input
-                            onChange={e => setStopLoss(e.target.value)}
-                            id="stopLoss"
-                            defaultValue={stopLoss}
-                            className="col-span-3"
-                        />
-                    </div>
-                    <div className="grid grid-cols-4 items-center gap-4">
-                        <Label htmlFor="takeProfit" className="text-right">
-                            Take Profit
-                        </Label>
-                        <Input
-                            onChange={e => setTakeProfit(e.target.value)}
-                            id="takeProfit"
-                            defaultValue={takeProfit}
-                            className="col-span-3"
-                        />
-                    </div>
+                        : <div className="grid gap-4 py-4">
+                            <div className="flex flex-col items-center gap-4">
+                                <Label htmlFor="message">Paste Trade Signal</Label>
+                                <Textarea className="col-span-3" placeholder="Paste trade signals here." id="tradesignals" defaultValue={textAreaText} onChange={e => setTextAreaText(e.target.value)} />
+                            </div>
+                        </div>
+                    }
                 </div>
                 <DialogFooter>
                     {isButtonDisabled ? <ButtonLoading /> : <Button onClick={isEditMode ? updateData : saveData}>{isEditMode ? "Update" : "Save"}</Button>}
+                    {/* <Button onClick={() => formatTextArea(textAreaText.split('\n'))}>Save</Button> */}
                 </DialogFooter>
             </DialogContent>
         </Dialog >
